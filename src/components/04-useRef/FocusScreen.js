8f8a6d2d55072989fc334c7d97d02fdc9d04f198
dieduran <diego.duran@gmail.com>
2021-03-22 17:27:04 -0300

    hasta video 113 completo

--- src/components/04-useRef/FocusScreen.js
+++ src/components/04-useRef/FocusScreen.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import '../02-useEffect/effect.css'
 
 export const FocusScreen = () => {
+
+    const inputRef = useRef()
+
+    const handleClick=() =>{
+        ////da foco y selecciona contenido
+        //document.querySelector('input').select();   //en lugar de .focus()
+        //con el ref no necesito buscarlo.. tengo la referencia
+        inputRef.current.select();
+        console.log(inputRef)
+    }
+
     return (
         <div>
             <h1>Focus Screen</h1>
-            <input
+            <input 
+                ref={inputRef}
                 className="form-control"
                 placeholder="Su nombre">
 
             </input>
-            <button className="btn btn-outline-primary mt-5">
+            <button className="btn btn-outline-primary mt-5"
+                onClick={handleClick}>
                 Focus
             </button>
         </div>
