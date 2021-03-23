d554fec008759c86041467a82c009d8a3011445a
dieduran <diego.duran@gmail.com>
2021-03-23 16:46:11 -0300

    hasta video 114

---
+++ src/components/05-useLayoutEffect/Layout.js
@@ -0,0 +1,44 @@
+import React,{ useLayoutEffect, useRef, useState } from 'react'
+import { useCounter } from '../../hooks/useCounter'
+import { useFetch } from '../../hooks/useFetch'
+import './layout.css'
+
+
+export const Layout = () => {
+    const {counter,increment}=useCounter(1);
+
+    const {data}= useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    //aca tengo si se cargo o esta esperando
+    //console.log(loading);
+
+    // sacar estos datos si data tiene valor (no es null) y lo saca del lugar 0
+    const {quote} = !!data && data[0];
+    const [boxSize, setBoxSize] = useState({})
+
+    const pTag = useRef();
+
+    useLayoutEffect(() => {
+        setBoxSize(pTag.current.getBoundingClientRect())
+    }, [quote])
+
+    return (
+        <div>
+            <h1>LayoutEffect: BreakingBad Quotes</h1>
+            <hr/>
+            <blockquote className="blockquote text-end"> 
+                <p 
+                    className="mb-3"
+                    ref={pTag}
+                >
+                    {quote}
+                </p>
+            </blockquote>
+            <pre>
+                {JSON.stringify( boxSize,null,3)}
+            </pre>
+            <button className='btn btn-primary' onClick={increment}>
+                Siguiente quote 
+            </button>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { FocusScreen } from './components/04-useRef/FocusScreen';
-import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+import { Layout } from './components/05-useLayoutEffect/Layout';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
@@ -10,6 +11,6 @@ import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <RealExampleRef />,
+    <Layout />,
   document.getElementById('root')
 );
