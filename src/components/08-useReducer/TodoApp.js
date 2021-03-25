1cf24396a993f7367c9b6e60e3e4cfabeb4b88b5
DiegoDuran <diego.duran@gmail.com>
2021-03-25 11:21:08 -0300

    agregado ejercicio 126

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -16,15 +16,44 @@ export const TodoApp = () => {
     
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ( {todos.length} ) </h1>
             <hr/>
+            <div className="row">
+                <div className="col-7">
+                    <ul className='list-group list-group-flush'>
+                    {
+                        todos.map( (todo,i)=>
+                            <li 
+                                key={todo.id}
+                                className="list-group-item"
+                            >
+                                <p className='text-center'>{i+1}. {todo.desc}</p>
+                                <button className="btn btn-danger">Borrar</button>
+                            </li>)
+                    }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TO DO</h4>
+                    <hr/>
+                    <form>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder="Aprender..."
+                            className="form-control"
+                            autoComplete="off"    
+                        />
+                        <button
+                            className="btn btn-outline-primary col-12" //no funciona mas el btn-block en bootstrap 5
+                        >
+                            Agregar
+                        </button>
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
+                    </form>
 
-            </ul>
+                </div>
+            </div>
 
         </div>
     )
