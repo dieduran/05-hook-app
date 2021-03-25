19357976a2cb8a812091cfd1c7cd33bfafa84021
DiegoDuran <diego.duran@gmail.com>
2021-03-25 10:13:50 -0300

    agregado ejercicio 125

---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,11 @@
+export const todoReducer = (state=[], action) =>{
+
+    switch (action.type) {
+        // case '':
+            
+        //     break;
+    
+        default: //se llama cuando se inicializar
+            break;
+    }
+}
