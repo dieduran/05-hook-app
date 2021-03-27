1d6952995f9ef4d833c7afcce2c21e2e10852841
DiegoDuran <diego.duran@gmail.com>
2021-03-27 12:58:43 -0300

    agregado el ejercicio 139

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -2,8 +2,10 @@ import React from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
   } from "react-router-dom";
+import { NavBar } from './NavBar';
 import { AboutScreen } from './AboutScreen';
 import { HomeScreen } from './HomeScreen';
 import { LoginScreen } from './LoginScreen';
@@ -12,11 +14,18 @@ export const AppRouter = () => {
     return (
         <Router>
            <div> 
+                <NavBar/>
                 <Switch>
                     <Route exact path="/" component={HomeScreen} />
                     <Route exact path="/about" component={AboutScreen} />
                     <Route exact path="/login" component={LoginScreen} />
-                </Switch>
+
+                    {/* //no encontro ninguna ruta
+                    <Route component={HomeScreen} /> */}
+                    // otra opcion
+                    <Redirect to="/" />
+
+                    </Switch>
             </div> 
         </Router>
     )
